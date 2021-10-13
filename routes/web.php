da26c026d91d983a5be3f47ad19b42c5eda0da24
devMahir <mahirshahriar10@gmail.com>
2021-10-14 01:25:11 +0600

    atthentication done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix'=>'admin', 'middleware'=>['admin','auth'],'namespace'=>'admin'], function(){
+    Route::get('dashboard', [AdminController::class, 'index']) -> name('admin.dashboard');
+});
+
+Route::group(['prefix'=>'user', 'middleware'=>['user','auth'],'namespace'=>'user'], function(){
+    Route::get('dashboard', [UserController::class, 'index']) -> name('user.dashboard');
+});
